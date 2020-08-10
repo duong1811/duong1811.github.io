a015f5dc82838fabb3d33af87d39f9d650b0ef8e
duong1811 <diemduong171@gmail.com>
2020-08-11 01:32:03 +0700

    clock

--- 1.js
+++ 1.js
@@ -1,61 +1,61 @@
 ﻿ $(function(){
-    function createSquare(){
-        const color = [
-            '#2196f3',
-            '#e91e63',
-            '#ffeb3b',
-            '74ff1d',
-        ]
-        const section = document.querySelector('body');
-        const square = document.createElement('span');
-        var size = Math.random()*50;
-        var height = $(window).height();
-        var width = $(window).width();
-        var auto1 = Math.random()*height;
-        var auto2 = Math.random()*width;
+    // function createSquare(){
+    //     const color = [
+    //         '#2196f3',
+    //         '#e91e63',
+    //         '#ffeb3b',
+    //         '74ff1d',
+    //     ]
+    //     const section = document.querySelector('body');
+    //     const square = document.createElement('span');
+    //     var size = Math.random()*50;
+    //     var height = $(window).height();
+    //     var width = $(window).width();
+    //     var auto1 = Math.random()*height;
+    //     var auto2 = Math.random()*width;
 
-        square.style.width = 20 + size + 'px';
-        square.style.height = 20 + size + 'px';
+    //     square.style.width = 20 + size + 'px';
+    //     square.style.height = 20 + size + 'px';
 
-        square.style.top = auto1 - size -20 +'px';
-        square.style.left = auto2 - size -20 +'px';
+    //     square.style.top = auto1 - size -20 +'px';
+    //     square.style.left = auto2 - size -20 +'px';
         
-        const bg = color[Math.floor(Math.random()*color.length)];
-        square.style.background = bg;
+    //     const bg = color[Math.floor(Math.random()*color.length)];
+    //     square.style.background = bg;
 
-        section.appendChild(square);
+    //     section.appendChild(square);
 
-        setTimeout(() =>{
-            square.remove()
-        }, 6000)
-     }
-      setInterval(createSquare, 150)
-      // end background
-     function clock(){
-        var hours = document.getElementById('Hours')
-        var minutes = document.getElementById('Minutes');
-        var seconds = document.getElementById('Seconds');
-        var day = document.getElementById('day');
-        var month = document.getElementById('month');
-        var year = document.getElementById('year');
+    //     setTimeout(() =>{
+    //         square.remove()
+    //     }, 6000)
+    //  }
+    //   setInterval(createSquare, 150)
+    //   // end background
+    //  function clock(){
+    //     var hours = document.getElementById('Hours')
+    //     var minutes = document.getElementById('Minutes');
+    //     var seconds = document.getElementById('Seconds');
+    //     var day = document.getElementById('day');
+    //     var month = document.getElementById('month');
+    //     var year = document.getElementById('year');
 
 
-        var h = new Date().getHours();
-        var m = new Date().getMinutes();
-        var s = new Date().getSeconds();
-        var d = new Date().getDate();
-        var mo = new Date().getMonth()+1;
-        var y = new Date().getFullYear();
+    //     var h = new Date().getHours();
+    //     var m = new Date().getMinutes();
+    //     var s = new Date().getSeconds();
+    //     var d = new Date().getDate();
+    //     var mo = new Date().getMonth()+1;
+    //     var y = new Date().getFullYear();
 
-        hours.innerHTML = h;
-        minutes.innerHTML = m;
-        seconds.innerHTML = s;
-        day.innerHTML = d;
-        month.innerHTML = mo;
-        year.innerHTML = y;
-    }
-        setInterval(clock, 1000)
-    // end time
+    //     hours.innerHTML = h;
+    //     minutes.innerHTML = m;
+    //     seconds.innerHTML = s;
+    //     day.innerHTML = d;
+    //     month.innerHTML = mo;
+    //     year.innerHTML = y;
+    // }
+    //     setInterval(clock, 1000)
+    // // end time
      
 })  
 
