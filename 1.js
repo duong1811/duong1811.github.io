433f44bc272a29d7d51b1a603b4398cfccddb18b
duong1811 <diemduong171@gmail.com>
2020-08-11 01:27:54 +0700

    clock

--- 1.js
+++ 1.js
@@ -6,19 +6,19 @@
             '#ffeb3b',
             '74ff1d',
         ]
-        const section = document.querySelector('section');
+        const section = document.querySelector('body');
         const square = document.createElement('span');
         var size = Math.random()*50;
-        var height = innerHeight;
-        var width = innerWidth;
+        var height = $(window).height();
+        var width = $(window).width();
         var auto1 = Math.random()*height;
         var auto2 = Math.random()*width;
 
         square.style.width = 20 + size + 'px';
         square.style.height = 20 + size + 'px';
 
-        square.style.top = auto1 + 'px';
-        square.style.left = auto2 + 'px';
+        square.style.top = auto1 - size -20 +'px';
+        square.style.left = auto2 - size -20 +'px';
         
         const bg = color[Math.floor(Math.random()*color.length)];
         square.style.background = bg;
@@ -29,6 +29,33 @@
             square.remove()
         }, 6000)
      }
-     setInterval(createSquare, 150)
+      setInterval(createSquare, 150)
+      // end background
+     function clock(){
+        var hours = document.getElementById('Hours')
+        var minutes = document.getElementById('Minutes');
+        var seconds = document.getElementById('Seconds');
+        var day = document.getElementById('day');
+        var month = document.getElementById('month');
+        var year = document.getElementById('year');
+
+
+        var h = new Date().getHours();
+        var m = new Date().getMinutes();
+        var s = new Date().getSeconds();
+        var d = new Date().getDate();
+        var mo = new Date().getMonth()+1;
+        var y = new Date().getFullYear();
+
+        hours.innerHTML = h;
+        minutes.innerHTML = m;
+        seconds.innerHTML = s;
+        day.innerHTML = d;
+        month.innerHTML = mo;
+        year.innerHTML = y;
+    }
+        setInterval(clock, 1000)
+    // end time
+     
 })  
- 
+
